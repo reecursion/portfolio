0152ecd2c8b4c00358eab15f06d864d61cdf6328
Gayathri <glgayathrii@gmail.com>
2023-08-14 17:06:08 +0530

    publication fix

--- components/scripts/publications.js
+++ components/scripts/publications.js
@@ -30,6 +30,7 @@ const publications = [{
                 authors: "<u>Gayathri G L</u>, Krithika Swaminathan, Divyasri K, Thenmozhi Durairaj, Bharathi B",
                 details: "Proceedings of the Second Workshop on Speech and Language Technologies for Dravidian Languages",
                 abstract: "Abusive language has lately been prevalent in comments on various social media platforms. The increasing hostility observed on the internet calls for the creation of a system that can identify and flag such acerbic content, to prevent conflict and mental distress. This task becomes more challenging when low-resource languages like Tamil, as well as the often-observed Tamil-English code-mixed text, are involved. The approach used in this paper for the classification model includes different methods of feature extraction and the use of traditional classifiers. We propose a novel method of combining language-agnostic sentence embeddings with the TF-IDF vector representation that uses a curated corpus of words as vocabulary, to create a custom embedding, which is then passed to an SVM classifier. Our experimentation yielded an accuracy of 52% and a macro F1-score of 0.54.",
+                status: "",
                 is_published: true
             },
             {
@@ -37,6 +38,7 @@ const publications = [{
                 authors: "Krithika Swaminathan, Hrishik Sampath, <u>Gayathri G L</u>, Bharathi B",
                 details: "Proceedings of the Second Workshop on Language Technology for Equality, Diversity and Inclusion",
                 abstract: "Over the years, there has been a slow but steady change in the attitude of society towards different kinds of sexuality. However, on social media platforms, where people have the license to be anonymous, toxic comments targeted at homosexuals, transgenders and the LGBTQ+ community are not uncommon. Detection of homophobic comments on social media can be useful in making the internet a safer place for everyone. For this task, we used a combination of word embeddings and SVM Classifiers as well as some BERT-based transformers. We achieved a weighted F1-score of 0.93 on the English dataset, 0.75 on the Tamil dataset and 0.87 on the Tamil-English Code-Mixed dataset.",
+                status: "",
                 is_published: true
             },
             {
@@ -44,6 +46,7 @@ const publications = [{
                 authors: "Divyasri K, <u>Gayathri G L</u>, Krithika Swaminathan, Thenmozhi Durairaj, Bharathi B, Senthil Kumar B",
                 details: "Proceedings of the Second Workshop on Speech and Language Technologies for Dravidian Languages",
                 abstract: "As the world around us continues to become increasingly digital, it has been acknowledged that there is a growing need for emotion analysis of social media content. The task of identifying the emotion in a given text has many practical applications ranging from screening public health to business and management. In this paper, we propose a language agnostic model that focuses on emotion analysis in Tamil text. Our experiments yielded an F1-score of 0.010.",
+                status: "",
                 is_published: true
             }
         ]
