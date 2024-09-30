6c086bcdf3e28a0f197ee44fa8aebfd2167ab1c1
Gayathri <85672063+reecursion@users.noreply.github.com>
2024-09-30 11:21:09 -0400

    portfolio updates

--- components/scripts/publications.js
+++ components/scripts/publications.js
@@ -1,4 +1,16 @@
 const publications = [{
+        year: 2024,
+        data: [{
+            title: "Cross-cultural Inspiration Detection and Analysis in Real and LLM-generated Social Media Data",
+            authors: "Oana Ignat, <u>Gayathri Ganesh Lakshmy</u>, Rada Mihalcea",
+            details: "",
+            abstract: "Inspiration is linked to various positive outcomes, such as increased creativity, productivity, and happiness. Although inspiration has great potential, there has been limited effort toward identifying content that is inspiring, as opposed to just engaging or positive. Additionally, most research has concentrated on Western data, with little attention paid to other cultures. This work is the first to study cross-cultural inspiration through machine learning methods. We aim to identify and analyze real and AI-generated cross-cultural inspiring posts. To this end, we compile and make publicly available the InspAIred dataset, which consists of 2,000 real inspiring posts, 2,000 real non-inspiring posts, and 2,000 generated inspiring posts evenly distributed across India and the UK. The real posts are sourced from Reddit, while the generated posts are created using the GPT-4 model. Using this dataset, we conduct extensive computational linguistic analyses to (1) compare inspiring content across cultures, (2) compare AI-generated inspiring posts to real inspiring posts, and (3) determine if detection models can accurately distinguish between inspiring content across cultures and data sources.",
+            status: "(Submitted to conference)",
+            url: "https://arxiv.org/abs/2404.12933",
+            is_published: true
+        }]
+    },
+    {
         year: 2023,
         data: [{
             title: "Segmentation and 3D reconstruction of the brain’s Grey matter, White matter and Cerebrospinal Fluid Regions",
@@ -9,21 +21,13 @@ const publications = [{
             url: "",
             is_published: false
         }, {
-            title: "GDP and Sector-wise Yield prediction using Time-Series Analysis",
-            authors: "<u>Gayathri G L</u>, Haricharan Bharathi",
-            details: "",
-            abstract: "Yet to be updated",
-            status: "(In Progress)",
-            url: "",
-            is_published: false
-        }, {
             title: "Abusive Comment Detection in Tamil Code-Mixed Data by Adjusting Class Weights and Refining Features",
             authors: "<u>Gayathri G L</u>, Krithika Swaminathan, Divyasri K, Thenmozhi Durairaj, Bharathi B",
-            details: "",
-            abstract: "Yet to be updated",
-            status: "(Under Review)",
-            url: "",
-            is_published: false
+            details: "ACM Transactions on Asian and Low-Resource Language Information Processing",
+            abstract: "In recent years, a significant portion of the content on various platforms on the internet has been found to be offensive or abusive. Abusive comment detection can go a long way in preventing internet users from facing the adverse effects of coming in contact with abusive language. This problem is particularly challenging when the comments are found in low-resource languages like Tamil or Tamil-English code-mixed text. So far, there has not been any substantial work on abusive comment detection using imbalanced datasets. Furthermore, significant work has not been performed, especially for Tamil code-mixed data, that involves analysing the dataset for classification and accordingly creating a custom vocabulary for preprocessing. This paper proposes a novel approach to classify abusive comments from an imbalanced dataset using a customised training vocabulary and a combination of statistical feature selection with language-agnostic feature selection while making use of explainable AI for feature refinement. Our model achieved an accuracy of 74% and a macro F1-score of 0.46.",
+            status: "",
+            url: "https://dl.acm.org/doi/abs/10.1145/3664619",
+            is_published: true
         }]
     },
     {
